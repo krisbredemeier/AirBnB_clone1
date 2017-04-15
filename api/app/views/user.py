45d36cb79dca1c0b607427476a0c5128e2dbd11f
Kris Bredemeier <34@holbertonschool.com>
2017-04-15 11:14:48 -0700

    suggested changes

--- api/app/views/user.py
+++ api/app/views/user.py
@@ -66,8 +66,6 @@ def create_user():
 	responses:
 		409:
 			description: email already exists
-
-
 	'''
 
 	try:
@@ -75,37 +73,48 @@ def create_user():
 			email=str(request.form['email']),
 			first_name=str(request.form['first_name']),
 			last_name=str(request.form['last_name']),
-			password=""
+			password=str(request.form['password'])
 		)
+		return jsonify(user)
 	except:
 		return jsonify({'code' : 10000, 'msg' : "Email already exists"}), 409
 
 
 @app.route('/users/<user_id>', methods=['GET'])
 def list_user_by_id(user_id):
-	user_ids = []
-	for user_id in User.select():
-		user_ids.append(user_id.to_hash())
-	return jasonify(user_ids)
+	'''
+	Get user by id
+	list of the given user using user_id in databse
+	---
+	tags: User
 
+
+	'''
+	user_ids = User.get(User.id == user_id)
+	try:
+		for user_id in User.select():
+			user_ids.append(user_id.to_hash())
+		return jasonify(user_ids)
+	except:
+		return jsonify({'msg' : abort}), 404
 
 @app.route('/users/<user_id>', methods=['PUT'])
 def update_user_by_id():
-	user_ids = []
+	user_ids = User.get(User.id == user_id)
 	for key in request.values:
 		if key == 'email':
 			return jsonify({'msg' : 'email can not be changed'}), 409
 		if key == 'updated_at' or key == 'created_at':
 			 continue
 		else:
-			 setattr(user_ids, key, request.values.get(key))
+			 setattr(user_found, key, request.values.get(key))
 	user.save()
-	return jsonify({'msg' : 'user sucessfuly updated'}), 200
+	return jsonify(user), 200
 
 
 @app.route('/users/<user_id>', methods=['DELETE'])
 def delete_user_by_id():
-	user_ids = []
+	user_ids = User.get(User.id == user_id)
 	try:
 		for user_id in User.select():
 			user_ids.delete_instance()
